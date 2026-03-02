f5188c8a4d5603cd0d9aa26c9b7d2a4dd703cf13
omar271909 <omar.aittalzif@gmail.com>
2026-03-02 13:18:42 +0100

    Update sendToHostinger with token header

--- sendToHostinger.js
+++ sendToHostinger.js
@@ -1,12 +1,33 @@
 const axios = require("axios");
 
-async function send(products){
+async function sendToHostinger({ plate, parts }) {
 
- await axios.post(
-   "https://tonsite.com/api/saveParts.php",
-   products
- );
+  const url = process.env.HOSTINGER_URL;
+  const token = process.env.HOSTINGER_TOKEN;
 
+  if (!url) {
+    throw new Error("HOSTINGER_URL not defined");
+  }
+
+  if (!token) {
+    throw new Error("HOSTINGER_TOKEN not defined");
+  }
+
+  const response = await axios.post(
+    url,
+    { plate, parts },
+    {
+      timeout: 15000,
+      headers: {
+        "Content-Type": "application/json",
+        "X-Auth-Token": token,
+      },
+    }
+  );
+
+  console.log("Hostinger response:", response.data);
+
+  return response.data;
 }
 
-module.exports = send;
+module.exports = sendToHostinger;
